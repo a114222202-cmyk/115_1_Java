645a366e54116c13aaba1be7cd75a89d434da407
a114222202-cmyk <a114222202@mail.shu.edu.tw>
2026-09-18 15:28:50 +0800

    第一周的課程，指令部分需要再確認

---
+++ java_01/HelloCourse.java
@@ -0,0 +1,7 @@
+public class HelloCourse {
+    public static void main(String[] args) {
+        // 固定輸出，方便比較不同工具的執行結果。
+        System.out.println("Hello, Java!");
+        System.out.println("Course: Object-Oriented Programming");
+    }
+}
